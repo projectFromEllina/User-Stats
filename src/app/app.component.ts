f5d01e12ded2d6c1734e737c27ace90f1ed78e76
Ellina <36732942+projectFromEllina@users.noreply.github.com>
2020-04-20 22:40:40 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
+  isShowPreloader: boolean = true;
+
+  constructor(private cdRef : ChangeDetectorRef) {
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.isShowPreloader = false;
+      this.cdRef.detectChanges();
+    }, 500)
   }
 }
